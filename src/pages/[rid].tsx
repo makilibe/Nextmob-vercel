4ab3c685f502586b934672cbc147ed51131cf961
makilibe <bk.y.bk.orange@gmail.com>
2021-03-04 14:05:50 +0900

    a

--- src/pages/[rid].tsx
+++ src/pages/[rid].tsx
@@ -4,7 +4,9 @@ import Peer from 'skyway-js';
 const peer = new Peer({ key: '00403e5e-fdf0-4ad6-bdf9-88c71127156f' });
 
 export default function room() {
-  const localStreamRef = useRef(null);
+  const localStreamRef = (useRef(null) as unknown) as React.MutableRefObject<
+    HTMLInputElement
+  >;
 
   let jsLocalStream;
   if (process.browser) {
