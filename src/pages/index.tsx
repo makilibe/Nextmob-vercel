1ff7d9b2b37585d80c6b30190bac067abc934978
makilibe <bk.y.bk.orange@gmail.com>
2021-04-25 15:19:16 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -59,7 +59,7 @@ const makeroom = () => {
   const [roomUUID, setRoomUUID] = useState('');
 
   const [currentUser, setCurrentUser] = useState<firebase.User | null>();
-  const [adminName, setAdminName] = useState('');
+  const [adminName, setAdminName] = useState<any | null>(null);
 
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
